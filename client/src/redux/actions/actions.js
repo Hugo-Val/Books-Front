152367b5a1b4221ec716f4921993cf8b54b55afc
Hugo Valencia <hvb1507@hotmail.com>
2023-07-18 20:47:24 -0600

    update

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,15 @@
+import * as types from './types';
+
+export const obtener_Todos_Libros = () => ({})
+export const obtener_Libro_Por_Id = () => ({})
+export const agregar_Libro = () => ({})
+export const eliminar_Libro = () => ({})
+export const actualizar_Libro = () => ({})
+export const filtrar_Libros = () => ({})
+export const quitar_Filtro = () => ({})
+export const ordenar_Libros = () => ({})
+export const quitar_Orden = () => ({})
+export const siguiente_Pagina = () => ({})
+export const pagina_Anterior = () => ({})
+export const agregar_Localizacion = () => ({})
+export const buscar_Libro_Por_Titulo = () => ({})
