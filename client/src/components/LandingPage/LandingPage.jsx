d08b49d6d5570cd192ccdfb275db8d82b7b3a092
Waldir merjildo <muwaldir@gmail.com>
2023-07-20 12:50:00 -0500

    avance

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -15,7 +15,7 @@ const LandingPage = () => {
       <header className="bg-light py-3">
         <div className="container d-flex align-items-center">
           <img src="https://img.freepik.com/vector-gratis/plantilla-logotipo-libreria-diseno-plano_23-2149325325.jpg?w=2000" alt="Logo de la tienda de libros" width="80" height="80" />
-          <h1 className="ms-4">Tu e-commerce de venta de libros</h1>
+          <h1 className="ms-4">Tu e-commerce de libros</h1>
         </div>
       </header>
       <main>
@@ -33,7 +33,7 @@ const LandingPage = () => {
         {/* Otras secciones de la landing page */}
       </main>
       <footer className="bg-light py-3 text-center">
-        <p className="fs-6 text-muted">Contacto: contacto@tu-tienda.com</p>
+        <p className="fs-6 text-muted">Contacto: contacto@books.com</p>
       </footer>
     </div>
   );
